c9bd54582ddb74e05f62c0d781fa4960e1c1adef
Danylo <dvityk@myseneca.ca>
2023-01-31 08:06:52 -0500

    dbs

---
+++ WS02/DIY/W2P2/W2P2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  W2P2
+//
+//  Created by Danik on 28.01.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

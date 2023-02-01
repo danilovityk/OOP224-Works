9e540d284e50ba78647226a3a8207d6e38c678d7
Danylo <dvityk@myseneca.ca>
2023-02-01 12:33:34 -0500

    Added comments

--- WS03/lab/Department.cpp
+++ WS03/lab/Department.cpp
@@ -1,3 +1,17 @@
+/*
+        Name: Danylo Vityk
+        Email: dvityk@myseneca.ca
+        Sudent ID: 176326213
+        Date: Feb 1, 2023
+
+
+ I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
+
+
+*/
+
+
+
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <cstring>

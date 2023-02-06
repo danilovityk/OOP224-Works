0ba69ec1df8e205202b110207abaad0262b136d5
Danylo <dvityk@myseneca.ca>
2023-02-06 12:59:48 -0500

    didnt start anything yet

---
+++ WS03/DIY/W3P2/W3P2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  W3P2
+//
+//  Created by Danik on 06.02.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

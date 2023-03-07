a59f2f1dde8db00819a04fce076efd808a852c80
Danylo <dvityk@gmail.com>
2023-03-07 13:46:27 -0500

    firt commit WS06

--- WS06/lab/Numbers.cpp
+++ WS06/lab/Numbers.cpp
@@ -1,7 +1,11 @@
+#include <iostream>
 #include <fstream>
+#include <iomanip>
 #include "Numbers.h"
 using namespace std;
 namespace sdds {
+
+
    void Numbers::sort(double* nums, unsigned int size) {
       unsigned int i, j;
       double temp;
@@ -24,4 +28,197 @@ namespace sdds {
       }
       return lines;
    }
+
+    Numbers::Numbers (char* filename)
+    {
+        if(!filename){
+            strcpy(m_filename, filename);
+            load();
+        }
+    }
+
+Numbers::Numbers() { 
+    setEmpty();
+}
+Numbers::~Numbers() {
+    save();
+    delete[] m_collection;
+}
+
+Numbers::Numbers(const sdds::Numbers &numbers) { 
+    this->setEmpty();
+    *this = numbers;
+    
+}
+
+sdds::Numbers Numbers::operator=(const sdds::Numbers &rOp) { 
+    if(*this != rOp)
+    {
+        save();
+        delete[] m_collection;
+        setEmpty();
+        if (rOp)
+        {
+            
+            m_original = false;
+            m_collection = new double[rOp.m_collectionSize];
+            for(int i = 0; i < rOp.m_collectionSize; i++)
+            {
+                m_collection[i] = rOp.m_collection[i];
+            }
+            m_collectionSize = rOp.m_collectionSize;
+
+        }
+    }
+    return *this;
+}
+
+Numbers::operator bool() const { 
+    return m_collection != nullptr;
+}
+
+void Numbers::sort() { 
+    sort(m_collection, m_collectionSize);
+}
+
+sdds::Numbers Numbers::operator+=(const double rOp) {
+    if(*this)
+    {
+        double *temp = new double[m_collectionSize + 1];
+        for (int i = 0; i < m_collectionSize; i++){
+            temp[i] = m_collection[i];
+        }
+        temp[m_collectionSize] = rOp;
+        delete[] m_collection;
+        m_collection = temp;
+        m_collectionSize++;
+    }
+    return *this;
+}
+
+std::ostream &Numbers::display(std::ostream &ostr) const { 
+    if(*this)
+    {
+        ostr<<fixed<<setprecision(2);
+        if (!m_original) ostr<<"Copy Of "<<m_filename<<endl;
+        for(int i = 0; i < m_collectionSize; i++)
+        {
+            ostr<<m_collection[i];
+        }
+        ostr<<endl << setfill('-') << setw(76);
+        ostr<<"Total of "<< m_collectionSize << " number(s), Largest: " << max() <<", Smallest: " << min() << ", Average: " << average()<<endl;
+        ostr<<endl << setfill('=') << setw(76);
+        
+    }else
+    {
+        ostr<<"Empty list";
+    }
+    return ostr;
+}
+
+void Numbers::setEmpty() {
+    
+    m_collection = 0;
+    m_filename[0] = '\0';
+    m_collectionSize = 0;
+    m_original = false;
+    m_addedFlag = false;
+}
+
+bool Numbers::load() {
+    unsigned int readLinesCount = countLines(m_filename);
+    
+    delete[] m_collection;
+    if(readLinesCount > 0)
+    {
+        unsigned int size = 0;
+        m_collection = new double[readLinesCount];
+        ifstream file(m_filename);
+        
+        
+        double temp;
+        while(file >> temp)
+        {
+            m_collection[size++] = temp;
+        }
+        
+        file.close();
+        
+        m_collectionSize = readLinesCount;
+        m_original = true;
+        
+        if (size != readLinesCount)
+        {
+            cout<<"\n\nWRONG SIZE OF LOAD\n\n";
+            delete[] m_collection;
+            setEmpty();
+        }
+        
+        
+    }
+    
+    return readLinesCount > 0;
+}
+
+void Numbers::save() {
+    if (m_original && m_collectionSize > 0)
+    {
+        ofstream file(m_filename);
+        
+        file << fixed << setprecision(2);
+        
+        for(int i = 0; i < m_collectionSize; i++){
+            file << m_collection[i] << endl;
+        }
+        
+        file.close();
+    }
+}
+
+double Numbers::max() const {
+    
+    double max = m_collection[0];
+    for (int i = 1; i < m_collectionSize; i ++)
+    {
+        if(max < m_collection[i]){
+            max = m_collection[i];
+        }
+    }
+    return max;
+}
+
+double Numbers::min() const {
+    double min = m_collection[0];
+    for (int i = 1; i < m_collectionSize; i ++)
+    {
+        if(min > m_collection[i]){
+            min = m_collection[i];
+        }
+    }
+    return min;
+}
+
+double Numbers::average() const {
+    double total = 0;
+    for (int i = 0; i < m_collectionSize; i ++)
+    {
+        total += m_collection[i];
+    }
+    return total/m_collectionSize;
+}
+
+
+istream& operator>>(istream istr, Numbers& right)
+{
+    double temp = 0;
+    istr >> temp;
+    right += temp;
+
+    return istr;
+}
+ostream& operator<<(ostream ostr, const Numbers& right)
+{
+   return  right.display(ostr);;
+}
+
 }
--- WS06/lab/Numbers.h
+++ WS06/lab/Numbers.h
@@ -1,12 +1,45 @@
 #ifndef SDDS_NUMBERS_H_
 #define SDDS_NUMBERS_H_
-#include <iostream>
+
+
 namespace sdds {
    class Numbers {
-      unsigned int countLines(const char* filename);
-      void sort(double* collectionPtr, unsigned int size);
+       
+       double* m_collection;
+       char m_filename[255];
+       unsigned int m_collectionSize;
+       bool m_original;
+       bool m_addedFlag;
+       
+       void setEmpty();
+       bool load();
+       void save();
+       double max() const;
+       double min() const;
+       double average() const;
+       
+       unsigned int countLines(const char* filename);
+       void sort(double* collectionPtr, unsigned int size);
+       
    public:
+       
+       Numbers (char* filename);
+       Numbers();
+       ~Numbers();
+       Numbers(const Numbers &numbers);
+       Numbers operator=(const Numbers& rOp);
+       operator bool() const;
+       void sort();
+       Numbers operator+=(const double rOp);
+       std::ostream& display(std::ostream& ostr = std::cout) const;
+       
+       
+      
+    
    };
+
+std::istream& operator>>(std::istream istr, Numbers& right);
+std::ostream& operator<<(std::ostream ostr, const Numbers& right);
 
 }
 #endif // !SDDS_NUMBERS_H_

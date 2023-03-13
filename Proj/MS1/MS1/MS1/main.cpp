ee341133594cc5143922510cbab3ab76d9853c43
Danylo <dvityk@gmail.com>
2023-03-13 16:20:08 -0400

    put proj into a folder

---
+++ Proj/MS1/MS1/MS1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  MS1
+//
+//  Created by Danik on 08.03.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

088bc1b3bf6d4c1feb1673f37aa31baebe1af491
Danylo <dvityk@gmail.com>
2023-03-14 12:19:27 -0400

    before pulling WS07

---
+++ Proj/MS2/MS2/MS2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  MS2
+//
+//  Created by Danik on 13.03.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

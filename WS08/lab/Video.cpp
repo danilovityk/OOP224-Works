0de2fb7fd63cb295d30c15c6fb0e21a08772a462
Danylo <dvityk@gmail.com>
2023-03-20 15:12:33 -0400

    Changed the directory of Media class

--- WS08/lab/Video.cpp
+++ WS08/lab/Video.cpp
@@ -1,19 +1,1 @@
-#include <iostream>
-#include "Media.h"
 
-using namespace std;
-
-namespace sdds{
-
-ostream& operator<<(ostream& ostr, const Media& media){
-    media.play(ostr);
-    
-    return ostr;
-}
-istream& operator>>(istream& istr, Media& media){
-    media.load(istr);
-    
-    return istr;
-}
-
-}

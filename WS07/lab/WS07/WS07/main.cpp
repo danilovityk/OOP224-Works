1731a090809ac3d3a436e388d56fe132600a8887
Danylo <dvityk@gmail.com>
2023-03-20 14:00:50 -0400

    da

---
+++ WS07/lab/WS07/WS07/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  WS07
+//
+//  Created by Danik on 14.03.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

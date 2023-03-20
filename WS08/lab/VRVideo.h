7dd9085dbe1f003ef601ac310fe8d29a72eef959
Danylo <dvityk@gmail.com>
2023-03-20 18:01:11 -0400

    Added the declaration

--- WS08/lab/VRVideo.h
+++ WS08/lab/VRVideo.h
@@ -1,4 +1,17 @@
 #ifndef VRVIDEO_H
+/*
+ *         Name: Danylo Vityk
+ *         Email: dvityk@myseneca.ca
+ *         Sudent ID: 176326213
+ *         Date: Mar 20, 2023
+ *
+ *
+ *         I have done all the coding by myself and only copied the code that my professor provided to complete my workshops
+ *         and assignments.
+ *
+ *
+ */
+
 #define VRVIDEO_H
 
 #include "Video.h"

37572d9f721bbfacdf88874d36f9323bc43298c7
Danylo <dvityk@gmail.com>
2023-03-27 15:27:29 -0400

    fixed the last line of output

--- Proj/MS3/Item.cpp
+++ Proj/MS3/Item.cpp
@@ -286,6 +286,7 @@ std::ifstream& Item::load(std::ifstream &ifstr) {
         strcpy(m_SKU, SKU);
         m_price = price;
         m_quantity = quantity;
+        m_flag = false;
         if (flag == 1) m_taxable = true;
         
     }

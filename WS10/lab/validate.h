8f3a51282d9a9b479ddd332dfef5c7db720b8a6d
Danylo <danilovityk@gmail.com>
2023-04-08 21:07:06 -0400

    Added declaration

--- WS10/lab/validate.h
+++ WS10/lab/validate.h
@@ -1,6 +1,15 @@
-#include <iostream>
-
-
+/*
+ *         Name: Danylo Vityk
+ *         Email: dvityk@myseneca.ca
+ *         Sudent ID: 176326213
+ *         Date: April 8, 2023
+ *
+ *
+ *         I have done all the coding by myself and only copied the code that my professor provided to complete my workshops
+ *         and assignments.
+ *
+ *
+ */
 
 template <typename type>
 
